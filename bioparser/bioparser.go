66511cd4e8eb1d1944a8234b69706d1481d3613e
Ric Arcila <ricartomojo@gmail.com>
2018-03-20 13:10:17 +0000

    Cleaning up, adding -h info on README

--- bioparser/bioparser.go
+++ bioparser/bioparser.go
@@ -1,10 +1,7 @@
 package bioparser
 
 import (
-	"fmt"
-
 	log "github.com/sirupsen/logrus"
-	"github.com/xeipuuv/gojsonschema"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -59,24 +56,4 @@ func printYaml() {
 		log.Panic(err)
 	}
 	log.Info(string(d))
-}
-
-func vali() {
-	schemaLoader := gojsonschema.NewReferenceLoader("file:///Users/arcila/ebi/dev/bioschemas-govalid/build/event.json")
-	documentLoader := gojsonschema.NewReferenceLoader("file:///Users/arcila/ebi/dev/bioschemas-govalid/build/tovalid.json")
-	log.Info(schemaLoader)
-
-	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if result.Valid() {
-		fmt.Printf("The document is valid\n")
-	} else {
-		fmt.Printf("The document is not valid. see errors :\n")
-		for _, desc := range result.Errors() {
-			fmt.Printf("- %s\n", desc)
-		}
-	}
 }
